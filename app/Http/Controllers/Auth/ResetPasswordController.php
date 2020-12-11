52dd892e18c4ac4e925f43a685b4d23c38ed10b7
vsereda <vladimir45822@gmail.com>
2020-12-11 18:39:24 +0200

    reset password controller fix

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -40,7 +40,7 @@ class ResetPasswordController extends Controller
      */
     public function showResetForm(Request $request, $token = null)
     {
-        return view('auth.passwords.reset')->with(
+        return view('auth.passwords.reset_soupculture')->with(
             ['token' => $token, 'email' => $request->email]
         );
     }
