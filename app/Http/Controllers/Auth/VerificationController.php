583a88a63dc322f8b6297798fc903c43984e0911
vsereda <vladimir45822@gmail.com>
2020-12-14 16:43:02 +0200

    verification was changed

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\VerifiesEmails;
+use Illuminate\Http\Request;
 
 class VerificationController extends Controller
 {
@@ -39,5 +40,19 @@ class VerificationController extends Controller
         $this->middleware('auth');
         $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
+    }
+
+    /**
+     * Show the email verification notice.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request)
+    {
+        return $request->user()->hasVerifiedEmail()
+            ? redirect($this->redirectPath())
+            : view('auth.verify_soupculture');
+//            : view('auth.verify');
     }
 }
---
+++ resources/views/auth/verify_soupculture.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app_soupculture')
+
+@section('content')
+    <main>
+        <div class="center_box">
+            <h2>{{ __('Verify Your Email Address') }}</h2>
+            <div id="login_wrapper">
+                <div class="verify_message_wrapper">
+                    @if (session('resent'))
+                    <h3 class="info-color">{{ __('A fresh verification link has been sent to your email address.') }}</h3>
+                    @endif
+                    <h3>{{ __('Before proceeding, please check your email for a verification link.') }}</h3>
+                    <h3>{{ __('If you did not receive the email') }},</h3>
+                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                        @csrf
+                        <button type="submit">
+                            {{ __('click here to request another') }}
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </main>
+@endsection
