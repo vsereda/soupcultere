2f2bf2095751e78db4450c2becc0e500e49a3d17
vsereda <vladimir45822@gmail.com>
2021-03-24 14:26:59 +0200

    SocialAuthController was moved to Auth folder

--- routes/web.php
+++ routes/web.php
@@ -17,19 +17,24 @@
 
 Auth::routes(['verify' => true]);
 
+Route::get('/test', 'TestController@test');
 
-Route::middleware(['verified.custom', 'check.user.address'])->group(function () {
+Route::middleware(['verified.custom', 'check.user.additional.data'])->group(function () {
     Route::get('/', 'HomeController@indexSoup')->name('home');
     Route::get('/menu', 'HomeController@menuSoup')->name('menu');
     Route::get('/delivery', 'HomeController@deliverySoup')->name('delivery');
     Route::get('/about', 'HomeController@aboutSoup')->name('about');
 });
 
-Route::get('/facebook/redirect', 'SocialAuthController@facebookRedirect');
-Route::get('/facebook/callback', 'SocialAuthController@facebookCallback');
+Route::namespace('Auth')->group(function (){
+    Route::get('/facebook/redirect', 'SocialAuthController@facebookRedirect');
+    Route::get('/facebook/callback', 'SocialAuthController@facebookCallback');
 
-Route::get('/google/redirect', 'SocialAuthController@googleRedirect');
-Route::get('/google/callback', 'SocialAuthController@googleCallback');
+    Route::get('/google/redirect', 'SocialAuthController@googleRedirect');
+    Route::get('/google/callback', 'SocialAuthController@googleCallback');
+});
 
-Route::get('/register/address', 'Auth\AddressController@registerForm')->name('address_register_form');
-Route::post('/register/address', 'Auth\AddressController@register')->name('address_register');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/register/additional', 'Auth\AdditionalDataController@registerForm')->name('address_register_form');
+    Route::post('/register/additional', 'Auth\AdditionalDataController@register')->name('address_register');
+});
