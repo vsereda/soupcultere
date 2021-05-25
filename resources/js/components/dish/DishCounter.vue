fe8afeb97ddb4f03b4c17d26e84e66a559b04c1e
vsereda <vladimir45822@gmail.com>
2021-05-25 16:00:31 +0300

    dishCounter plus/minus buttons works

--- resources/js/components/dish/DishCounter.vue
+++ resources/js/components/dish/DishCounter.vue
@@ -1,12 +1,18 @@
 <template>
-<div class="dish-counter-template">
-    <div class="dish-counter-template-row">
-        <span>-</span>
-        <input v-model="count">
-        <span>+</span>
+    <div class="dish-counter-template">
+        <div class="dish-counter-template-row">
+            <span @click="minus()">
+                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                width="124px" height="124px" viewBox="0 0 124 124" style="enable-background:new 0 0 124 124;" xml:space="preserve"><g>	<path d="M112,50H12C5.4,50,0,55.4,0,62c0,6.6,5.4,12,12,12h100c6.6,0,12-5.4,12-12C124,55.4,118.6,50,112,50z"/></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>
+            </span>
+            <input v-model="count">
+            <span @click="plus()">
+                <svg height="24px" viewBox="0 0 448 448" width="24px" xmlns="http://www.w3.org/2000/svg"><path
+                    d="m408 184h-136c-4.417969 0-8-3.582031-8-8v-136c0-22.089844-17.910156-40-40-40s-40 17.910156-40 40v136c0 4.417969-3.582031 8-8 8h-136c-22.089844 0-40 17.910156-40 40s17.910156 40 40 40h136c4.417969 0 8 3.582031 8 8v136c0 22.089844 17.910156 40 40 40s40-17.910156 40-40v-136c0-4.417969 3.582031-8 8-8h136c22.089844 0 40-17.910156 40-40s-17.910156-40-40-40zm0 0"/></svg>
+            </span>
+        </div>
+        <p>{{ servingTitle }} - 86 грн</p>
     </div>
-    <p>{{ servingTitle }} - 86 грн</p>
-</div>
 </template>
 
 <script>
@@ -29,90 +35,35 @@ export default {
     data: function () {
         return {
             count: 0,
-            minCount: 0,
-            maxCount: 1000,
+        }
+    },
+    methods: {
+        plus: function () {
+            this.count++;
+        },
+        minus: function () {
+            this.count--;
         }
     },
     watch: {
         count: function (newVal, oldVal) {
             let newCount = parseInt(newVal);
-            let absNewCount = (count) => Math.abs(count);
             let formatMinMaxCont = function (count, min, max) {
                 if (count < min) {
                     return min;
-                }
-                else if (count > max)
-                {
+                } else if (count > max) {
                     return max;
                 }
                 return count;
             }
-            this.count = (Number.isNaN(newCount) ? 0 : formatMinMaxCont(absNewCount(newCount), this.minCount, this.maxCount));
+            let resultCount = (Number.isNaN(newCount) ? this.minCount : formatMinMaxCont(newCount, this.minCount, this.maxCount));
+            this.count = resultCount;
+            this.$emit('countChange', this.count);
         },
     }
 }
 </script>
 
 <style scoped>
-    .dish-counter-template {
-        margin: 0;
-        padding: 15px 0 0 0;
-    }
-
-    .dish-counter-template p {
-        padding-top: 4px;
-    }
-
-    .dish-counter-template-row {
-        display: flex;
-        flex-direction: row;
-    }
-
-    .dish-counter-template span,
-    .dish-counter-template input {
-        display: flex;
-        font-size: 24px;
-        border: 2px solid #b5692a;
-        padding-top: 5px;
-        padding-bottom: 5px;
-        background: rgba(0, 0, 0, 0);
-        color: inherit;
-        outline: none;
-    }
-
-    .dish-counter-template span:first-child,
-    .dish-counter-template span:last-child {
-        padding-left: 20px;
-        padding-right: 20px;
-        cursor: pointer;
-    }
-
-    .dish-counter-template span:first-child:hover,
-    .dish-counter-template span:last-child:hover {
-        color: #ffed4a;
-    }
-
-    .dish-counter-template span:first-child:active,
-    .dish-counter-template span:last-child:active {
-        border-color: rgba(181, 105, 42, 0.5);
-    }
-
-    .dish-counter-template span:first-child {
-        border-bottom-left-radius: 22px;
-        border-top-left-radius: 22px;
-        border-right: 0;
-    }
-
-    .dish-counter-template span:last-child {
-        border-bottom-right-radius: 22px;
-        border-top-right-radius: 22px;
-        border-left: 0;
-    }
-
-    .dish-counter-template input {
-        display: inline;
-        text-align: center;
-        width: 70px;
-    }
 
 </style>
